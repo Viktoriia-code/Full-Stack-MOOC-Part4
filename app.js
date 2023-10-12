edc71dc9c7672cebf4dc32996f1a7e34df87244e
Viktoriia <beloborodova.viktoriia@gmail.com>
2023-10-12 18:13:58 +0300

    organized app file

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const middleware = require('./utils/middleware')
-const usersRouter = require('./controllers/users')
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -32,11 +39,6 @@ app.use(middleware.userExtractor)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
-
-if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
-}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
